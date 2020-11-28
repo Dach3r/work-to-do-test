138ef3776acda11399d50a3927bd379bdfce2311
Dach3r <dach3r@gmail.com>
2020-11-28 18:21:18 -0500

    internalization url

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users, defaults: { format: :json }, controllers: {
-    sessions: 'v1/auth/sessions',
-    registrations: 'v1/auth/registrations',
-    passwords: 'v1/auth/passwords'
-  }
+  scope "(:locale)", locale: /en|es/ do
+    devise_for :users, defaults: { format: :json }, controllers: {
+      sessions: 'v1/auth/sessions',
+      registrations: 'v1/auth/registrations',
+      passwords: 'v1/auth/passwords'
+    }
 
-  namespace :v1 do
-    resources :categories, only: %w[index create]
-    resources :projects, only: %w[create update show] do
-      member do
-        get :pending
-        get :users
+    namespace :v1 do
+      resources :categories, only: %w[index create]
+      resources :projects, only: %w[create update show] do
+        member do
+          get :pending
+          get :users
 
-        post :add_user
-      end
+          post :add_user
+        end
 
-      resources :tasks, only: %w[index show create update] do
-        resources :subtasks, only: :create
-        put :update_status, on: :member
+        resources :tasks, only: %w[index show create update] do
+          resources :subtasks, only: :create
+          put :update_status, on: :member
+        end
       end
     end
   end
